07ac761ba2cd7aa2fcbf7181398f6a6e6fa678cf
Ahmad salman <ahmadg.salman@hotmail.com>
2021-11-09 14:22:50 +0200

    Initial Commit

---
+++ SwiftUIFirstProject/SwiftUIFirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirstProjectApp.swift
+//  SwiftUIFirstProject
+//
+//  Created by Macbookbro on 11/9/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
