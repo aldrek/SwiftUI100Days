72d9dd57987a76a57b477b9b1b394c4a2d2f5ccc
Ahmad salman <ahmadg.salman@hotmail.com>
2021-11-10 12:12:30 +0200

    Finish swiftUI part 2

--- SwiftUIFirstProject/SwiftUIFirstProjectApp.swift
+++ SwiftUIFirstProject/SwiftUIFirstProjectApp.swift
@@ -11,9 +11,7 @@ import SwiftUI
 struct SwiftUIFirstProjectApp: App {
     var body: some Scene {
         WindowGroup {
-//            TapCount()
-//            BindingState()
-            CreatingViewsthroughLoop()
+            WeSplit()
         }
     }
 }
